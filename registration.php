fb65a05502db5088a441bf811e1aebe78085bfbc
Francois Raubenheimer <fran@fontera.com>
2016-06-07 19:42:29 +0200

    Prototype release

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright (c) 2016 CoisIO. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'CoisIO_M2DevEnv',
+    __DIR__
+);
